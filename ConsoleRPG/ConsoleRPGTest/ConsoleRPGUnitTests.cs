f8aa85d0c633b26c2e2c2b439f152a796fec21be
s325909 <jaso_cyril@hotmail.com>
2022-01-17 12:20:28 +0100

    Chore: initial project and unit tests

---
+++ ConsoleRPG/ConsoleRPGTest/ConsoleRPGUnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ConsoleRPGTest
+{
+    public class ConsoleRPGUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
